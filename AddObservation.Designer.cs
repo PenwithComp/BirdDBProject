9d9309b2d08541e94ed5ca108fa10e817bb99624
Jamie Nevill <jamienevill@truro-penwith.ac.uk>
2021-02-08 05:19:38 +0000

    Add project files.

---
+++ AddObservation.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace BirdDBProject
+{
+    partial class AddObservation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.spieciesLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(453, 347);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(115, 60);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // spieciesLbl
+            // 
+            this.spieciesLbl.AutoSize = true;
+            this.spieciesLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spieciesLbl.Location = new System.Drawing.Point(69, 44);
+            this.spieciesLbl.Name = "spieciesLbl";
+            this.spieciesLbl.Size = new System.Drawing.Size(60, 20);
+            this.spieciesLbl.TabIndex = 1;
+            this.spieciesLbl.Text = "Spicies";
+            // 
+            // AddObservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.spieciesLbl);
+            this.Controls.Add(this.addBtn);
+            this.Name = "AddObservation";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.AddObservation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label spieciesLbl;
+    }
+}
+
